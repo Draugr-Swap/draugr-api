de52fcdc391df43bd989fa7d7f6fe33b2f6a594b
Naoki Murano <naokimurano@outlook.com>
2022-02-24 03:12:30 +0900

    update contract addresses

--- configs/ContractAddresses.js
+++ configs/ContractAddresses.js
@@ -1,4 +1,4 @@
 export const ContractAddresses = {
-    masterChef: "0x9DFf893BBCE5E1aFe42FffcB4cbceFcF644330ff",
-    multiCall: "0x6eEa1D600e42D54A720a8E1b13db036559dD07D1",
+    masterChef: "0xECe0Ce43D68d52F85988Fc3Bf53815E0812A6662",
+    multiCall: "0x760BF09f2b1f3a40144b7Fe999e9F619DE5f8A31",
 }
